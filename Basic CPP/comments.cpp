87c493f3e7c506d76702563891f04df42b051c54
nahid-105-ontor <ontornahidhasan65@gmail.com>
2026-09-08 12:40:25 +0600

    Successfully Learned Comments

---
+++ Basic CPP/comments.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Comments are Twu types:"<<endl;
+    cout<<"1)Single Line Comment"<<endl;
+    cout<<"2)Multiline Comment"<<endl;
+    return 0;
+}

5e24b022d77874499a1e411c239498ffd2e454da
nahid-105-ontor <ontornahidhasan65@gmail.com>
2026-09-08 12:35:57 +0600

    Successfully Taking Input

--- Basic CPP/input.cpp
+++ Basic CPP/input.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main()
 {
-    
+    int age;
+    cout<<"Enter Your Age = ";
+    cin>>age;
+    cout<<"Your Age is : "<<age<<endl;
+    cout<<"Thank You";
+    return 0;
 }
